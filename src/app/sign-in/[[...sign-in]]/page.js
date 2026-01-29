af248e2e97ad7484c0e33d187e1e3118f121bf43
ERIK J CLARK <ERIKJCLARK@ICLOUD.COM>
2026-01-29 13:28:55 +0000

    chore: file fill template

--- src/app/sign-in/[[...sign-in]]/page.js
+++ src/app/sign-in/[[...sign-in]]/page.js
@@ -1,2 +1,15 @@
 //TODO: render the sign-in page
 //- Use Clerk component
+ 
+export default function SignUpPage() {
+  return (
+    <>
+      {/* Clerk component */}
+      <form>
+        <input type="text" name="nickname" />
+        <textarea type="text" name="bio" />
+        <input type="number" name="age" />
+      </form>
+    </>
+  );
+}
